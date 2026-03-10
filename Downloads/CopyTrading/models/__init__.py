94663177f68b7a369adc08e5fcbd370acee49e36
matthewmoyer21774 <moyer.matthewd@gmail.com>
2026-03-10 10:32:35 +0100

    Delete Downloads/CopyTrading directory

--- Downloads/CopyTrading/models/__init__.py
+++
@@ -1,5 +0,0 @@
-from .trade import Trade
-from .position import Position
-from .trader import Trader, TraderStats, EquityCurvePoint
-
-__all__ = ['Trade', 'Position', 'Trader', 'TraderStats', 'EquityCurvePoint']
--- Downloads/CopyTrading/services/__init__.py
+++
@@ -1,5 +0,0 @@
-from .trader_discovery import TraderDiscoveryService
-from .trader_analyzer import TraderAnalyzerService
-from .equity_curve import EquityCurveBuilder
-
-__all__ = ['TraderDiscoveryService', 'TraderAnalyzerService', 'EquityCurveBuilder']
